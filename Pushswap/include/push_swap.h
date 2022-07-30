c1c41c7ba56c3ce8903404803692be61732f63af
Furkan Yardımcı <92301220+Feanor1021@users.noreply.github.com>
2022-07-30 14:09:56 +0300

    Add files via upload

--- Pushswap/include/push_swap.h
+++ Pushswap/include/push_swap.h
@@ -13,12 +13,13 @@ typedef struct push
     struct push *prev;
 }t_list;
 
-
+//mandatory fuctions
 t_list  *create_stack_node( long num);
 int add_node_to_stack(t_list **root, t_list **tail, t_list *temp);
 int stack_create_and_control(t_list **root, t_list **tail,char **argv);
 t_list* pop_stack(t_list **root, t_list **tail);;
 void print_list(t_list *root, t_list *tail);
+int ft_isspace_srch_all(char *argv_sub);
 
 //rotates
 void shift_up_r(t_list **root, t_list **tail,char id);
@@ -31,8 +32,11 @@ void push_a(t_list **root , t_list **tail, t_list **root2 , t_list **tail2);
 void swap_both( t_list **tail, t_list **tail2);
 void swap_s( t_list **tail,char id);
 
-
+//libft_functions
 t_list*   ft_atol(const char *str);
 char	**ft_split(char const *s, char c);
+char	*ft_substr(char const *s1, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
 
 #endif
