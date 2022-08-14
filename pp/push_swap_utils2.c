6d23db56bf5381df15cdf794306c30de7e57972b
Furkan Yardımcı <92301220+Feanor1021@users.noreply.github.com>
2022-08-14 18:31:11 +0300

    Add files via upload

---
+++ pp/push_swap_utils2.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_utils2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyardimc <fyardimc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/14 15:45:35 by fyardimc          #+#    #+#             */
+/*   Updated: 2022/08/14 15:45:48 by fyardimc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+char	*ft_substr(char const *s1, unsigned int start, size_t len)
+{
+	char			*arry;
+	unsigned int	size_s1;
+
+	if (!s1)
+		return (NULL);
+	size_s1 = ft_strlen(s1);
+	if (start >= ft_strlen(s1))
+	{
+		arry = (char *)malloc(sizeof(char));
+		if (!arry)
+			return (NULL);
+		*arry = '\0';
+		return (arry);
+	}
+	arry = (char *)malloc(sizeof(char) * (len + 1));
+	if (!arry)
+		return (arry);
+	ft_strlcpy(arry, (char *)(s1 + start), len + 1);
+	return (arry);
+}
+
+static void	ft_allocate(char **tab, char const *s, char sep)
+{
+	char		**tab_p;
+	char const	*tmp;
+
+	tmp = s;
+	tab_p = tab;
+	while (*tmp)
+	{
+		while (*s == sep)
+			++s;
+		tmp = s;
+		while (*tmp && *tmp != sep)
+			++tmp;
+		if (tmp > s)
+		{
+			*tab_p = ft_substr(s, 0, tmp - s);
+			s = tmp;
+			++tab_p;
+		}
+	}
+	*tab_p = NULL;
+}
+
+static int	ft_count_words(char const *s, char sep)
+{
+	int	word_count;
+
+	word_count = 0;
+	while (*s)
+	{
+		while (*s == sep)
+			++s;
+		if (*s)
+			++word_count;
+		while (*s && *s != sep)
+			++s;
+	}
+	return (word_count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**new;
+	int		size;
+
+	if (!s)
+		return (NULL);
+	size = ft_count_words(s, c);
+	new = (char **)malloc(sizeof(char *) * (size + 1));
+	if (!new)
+		return (NULL);
+	ft_allocate(new, s, c);
+	return (new);
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	counter;
+
+	if (n == 0)
+		return (0);
+	counter = 0;
+	while (counter < (n - 1) && s1[counter] && s2[counter])
+	{
+		if (s1[counter] != s2[counter])
+			break ;
+		counter++;
+	}
+	return (((unsigned char *)s1)[counter] - ((unsigned char *)s2)[counter]);
+}
